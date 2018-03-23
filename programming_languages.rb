e1c69db7dab61e0cfa6b434af488def23d89f496
Daniel Brazel <dbrazel4@gmail.com>
2018-03-23 14:42:45 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -6,7 +6,7 @@ def reformat_languages(languages)
     langs.each do |lang, type|
       type.each do |el1, el2|
         binding.pry
-        answer[lang] = {type, style: style}
+        # answer[lang] = {type, style: style}
       end
     end
   end

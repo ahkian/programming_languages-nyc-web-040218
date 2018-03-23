4049dceb6a0ef536b82a98b1ed35c2e8a997de55
Daniel Brazel <dbrazel4@gmail.com>
2018-03-23 14:36:33 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,7 +3,7 @@ require 'pry'
 def reformat_languages(languages)
   answer = Hash.new
   languages.each do |style, langs|
-    langs.each do |el1, el2|
+    langs.each do |lang, type|
       binding.pry
     end
   end

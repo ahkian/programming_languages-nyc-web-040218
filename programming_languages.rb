447ffc36f7411a630d4a026685a4878750caacc0
Daniel Brazel <dbrazel4@gmail.com>
2018-03-23 14:40:58 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,7 +4,7 @@ def reformat_languages(languages)
   answer = Hash.new
   languages.each do |style, langs|
     langs.each do |lang, type|
-      answer[lang] = {type: type, style: style}
+      answer[lang] = {type, style: style}
     end
   end
   p answer
